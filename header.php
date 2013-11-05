1bf5e9d60afb1236cc969eb07bbd31a0356d11b4
Nishant Gupta <ccsCoder@gmail.com>
2013-11-05 21:04:48 +0530

    -- Coded a new Menu to go with the Look and feel of the Website. -- Made it responsive -- Corrected styles to be consistent -- removed a lot of inline styles -- Removed duplicate jQuery loads

---
+++ header.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en" style="-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+        <title>Technoflair Lab</title>
+        <link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="css/flexdropdown.css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css' />
+
+        <link href='css/sm-core-css.css' rel='stylesheet' type='text/css' />
+        <link href='css/sm-clean/sm-clean.css' rel='stylesheet' type='text/css' />
+        
+        <script src="js/jquery-1.8.0.min.js" type="text/javascript"></script>
+        <!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>-->
+        <!--<script type="text/javascript" src="js/flexdropdown.js"></script>-->
+        <!--[if lt IE 9]>
+                <script src="js/modernizr.custom.js"></script>
+        <![endif]-->
+        
+        <script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
+        <script src="js/functions.js" type="text/javascript"></script>
+        <script src="js/jquery.smartmenus.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+        <!-- wraper -->
+        <div id="wrapper">
+            <!-- shell -->
+            <div class="shell">
+                <!-- container -->
+                <div class="container">
+                    <!-- header -->
+                    <header id="header">
+                        <h1 id="logo"><a href="#">Technoflair Lab</a></h1>
+                        <!-- search -->
+                        <div class="search">
+                            <form method="post">
+                                <span class="field"><input type="text" class="field" value="keywords here ..." title="keywords here ..." /></span>
+                                <input type="submit" class="search-btn" value="" />
+                            </form>
+                        </div>
+                        <!-- end of search -->
+                    </header>
+                    <!-- end of header -->
+                    <!-- navigation -->
+                    <div class="nav">
+                        <!--<a href="#" class="nav-btn" data-flexmenu="flexmenu1"><span style="color:#6FA632">ABOUT</span><span class="arr"></span></a>-->
+                        <ul id="navigationMenu" class="sm sm-clean">
+                            <li class="nav-menu_item_neo">
+                                <a href="#"><span>about</span></a>
+                                <ul >
+                                    <li ><a href="main.php?f=what_we_do">What we do?</a></li>
+                                    <li ><a href="main.php?f=how_we_do">How we do?</a></li>
+                                    <li ><a href="main.php?f=who_we_are">Who we are?</a></li>
+                                </ul>
+                            </li>
+                            
+                            <li class="nav-menu_item_neo">
+                                 <a href="#">e-Lab</a>
+                                 <ul >
+                                    <li ><a href="main.php?f=research_development">Research & Development</a>
+                                        <ul >
+                                            <li ><a href="main.php?f=innovation">Innovation</a>
+                                                <ul >
+                                                    <li ><a href="main.php?f=about_ideas">Team-Passionate about ideas</a></li>
+                                                    <li ><a href="main.php?f=helping_to_buid_business">Helping to buid business</a></li>
+                                                </ul>
+                                            </li>
+                                            <li ><a href="main.php?f=solution_for_you">Solution for you</a></li>
+                                        </ul>
+                                    </li>
+                                    <li ><a href="main.php?f=e_recovery">e-Recovery</a>
+                                        <ul >
+                                            <li ><a href="main.php?f=data">Data</a>
+                                                <ul >
+                                                    <li ><a href="main.php?f=recover_your_disk_data">Recover your Hard Disk data</a></li>
+                                                    <li ><a href="main.php?f=recover_your_laptop">Recover your laptop OS</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            
+                            <li class="nav-menu_item_neo">
+                                <a href="#">e-Technologies</a>
+                                <ul >
+                                    <li ><a href="main.php?f=business_intelligence">Business Intelligence</a>
+                                        <ul >
+                                            <li ><a href="main.php?f=ibm_cognos">IBM Cognos</a>
+                                                <ul >
+                                                    <li ><a href="main.php?f=dashboarding">Dashboarding</a></li>
+                                                    <li ><a href="main.php?f=mobility">Mobility</a></li>
+                                                </ul>
+                                            </li>
+                                            <li ><a href="main.php?f=roambi">ROAMBI</a></li>
+                                            <li ><a href="main.php?f=qlik_view">Qlik View</a></li>
+                                            <li ><a href="main.php?f=tm1">TM1</a></li>
+                                        </ul>
+                                    </li>
+                                    <li ><a href="main.php?f=cloud_computing">Cloud Computing</a></li>
+                                    <li ><a href="main.php?f=infrastructure_services">Infrastructure Services</a>
+                                        <ul >
+                                            <li ><a href="main.php?f=platform_designing">Platform Designing</a></li>
+                                            <li ><a href="main.php?f=virtualization">Virtualization</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-menu_item_neo">
+                                <a href="#">e-Laboratory</a>
+                                <ul >
+                                    <li ><a href="main.php?f=order_now">Order Now!-CBT</a></li>
+                                    <li ><a href="main.php?f=online_training">Online Training</a></li>
+                                    <li ><a href="main.php?f=career_counselling">Career Counselling</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-menu_item_neo">
+                                <a href="#">e-Consulting</a>
+                                <ul>
+                                    <li ><a href="main.php?f=business_consulting">Business Consulting</a></li>
+                                    <li ><a href="main.php?f=it_consulting">IT Consulting</a>
+                                        <ul >
+                                            <li ><a href="main.php?f=manage_support_services">Manage Support Services</a></li>
+                                            <li ><a href="main.php?f=staff_augmentation">Staff augmentation</a></li>
+                                        </ul>
+                                    </li>
+                                    <li ><a href="main.php?f=product_consulting">Product Consulting</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-menu_item_neo">
+                                <a href="#">i-Connect</a>
+                                <ul >
+                                    <li ><a href="main.php?f=worldwide">Worldwide</a></li>
+                                    <li ><a href="main.php?f=references">References</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <!-- end of navigation -->
+                    <!-- slider -->
+                    <div class="m-slider">
+                        <div class="slider-holder">
+                            <span class="slider-shadow"></span>
+                            <span class="slider-b"></span>
+                            <div class="slider flexslider">
+                                <ul class="slides">
+                                    <li>
+                                        <div class="img-holder">
+                                            <img src="css/images/slide-img1.png" alt="" />
+                                        </div>
+                                        <div class="slide-cnt">
+                                            <h2>E-Technologies</h2>
+                                            <div class="box-cnt">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget augue quis lorem ipsum dolor sit amet, consectetur adipiscing elit. llus eget augue quis lorem ipsum dolor sit amet, free css templates</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="img-holder">
+                                            <img src="css/images/slide-img2.png" alt="" />
+                                        </div>
+                                        <div class="slide-cnt">
+                                            <h2>E-Laboratory</h2>
+                                            <div class="box-cnt">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget augue quis lorem ipsum dolor sit amet, consectetur adipiscing elit. llus eget augue quis lorem ipsum dolor sit amet, free css templates</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="img-holder">
+                                            <img src="css/images/slide-img1.png" alt="" />
+                                        </div>
+                                        <div class="slide-cnt">
+                                            <h2>E-Consulting</h2>
+                                            <div class="box-cnt">
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget augue quis lorem ipsum dolor sit amet, consectetur adipiscing elit. llus eget augue quis lorem ipsum dolor sit amet, free css templates</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>		
+                    <!-- end of slider -->
+                    <!-- main -->
+                    <div class="main">
