eec348da0da44c5af41c9ea7c6123b15d885b08d
Nishant Gupta <ccsCoder@gmail.com>
2013-11-06 13:11:59 +0530

    -- Created pluggable page framents in PHP, to enable re-use across multiple pages in the website. -- Composited index.php from them.

---
+++ comp/header.php
@@ -0,0 +1,20 @@
+<?php 
+    /**
+     * Header php fragment
+     * 
+     */
+?>
+
+<!-- header -->
+<header id="header">
+    <h1 id="logo"><a href="#">Technoflair Lab</a></h1>
+    <!-- search -->
+    <div class="search">
+        <form method="post">
+            <span class="field"><input type="text" class="field" value="keywords here ..." title="keywords here ..." /></span>
+            <input type="submit" class="search-btn" value="" />
+        </form>
+    </div>
+    <!-- end of search -->
+</header>
+<!-- end of header -->
---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en" style="-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+        <title>Technoflair Lab</title>
+        <link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="css/flexdropdown.css" />
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+        <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css' />
+
+        <link href='css/sm-core-css.css' rel='stylesheet' type='text/css' />
+        <link href='css/sm-clean/sm-clean.css' rel='stylesheet' type='text/css' />
+
+        <script src="js/jquery-1.8.0.min.js" type="text/javascript"></script>
+        <!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>-->
+        <!--<script type="text/javascript" src="js/flexdropdown.js"></script>-->
+        <!--[if lt IE 9]>
+                <script src="js/modernizr.custom.js"></script>
+        <![endif]-->
+
+        <script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
+        <script src="js/functions.js" type="text/javascript"></script>
+        <script src="js/jquery.smartmenus.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+        <div id="wrapper">
+            <!-- shell -->
+            <div class="shell">
+                <!-- container -->
+                <div class="container">
+                    <?php include 'comp/header.php'; ?>
+                    <?php include 'comp/nav.php' ?>
+                    <?php include 'comp/slider.php' ?>
+                    <div class="main">
+                        <section class="cols">
+                            <div class="col">
+                                <img src="css/images/col-img1.png" alt="" />
+                                <div class="col-cnt">
+                                    <h2>Support</h2>
+                                    <p>We provide support in Business Intelligence(Administration and Report Development)</p>
+                                    <a href="#" class="more">view more</a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <img src="css/images/col-img2.png" alt="" />
+                                <div class="col-cnt">
+                                    <h2>Training</h2>
+                                    <p>We provide customized <strong>'Online and Classroom'</strong> Training</p>
+                                    <a href="#" class="more">view more</a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <img src="css/images/col-img3.png" alt="" />
+                                <div class="col-cnt">
+                                    <h2>Consulting</h2>
+                                    <p>We provide services in <strong>design, architecture, Installation, Development and implementation</strong> <!--of data warehouse and other business intelligence solutions. If you are considering modifying or building a data warehouse for your organization or if your company is in need of a high level of staffing service, please explore our website and <strong>contact us</strong> for more information about <strong>what we can do for you!</strong>--> </p>
+                                    <a href="#" class="more">view more</a>
+                                </div>
+                            </div>
+                            <div class="cl">&nbsp;</div>
+                        </section>
+                        <section class="post">
+                            <div class="video-holder">
+                                <iframe width="100%" height="265" src="//www.youtube.com/embed/dlJshzOv2cw" frameborder="0" allowfullscreen></iframe>
+                            </div>
+
+                            <div class="post-cnt">
+                                <h2>A smarter planet!</h2>
+                                <p><strong>A smarter planet begins with smarter educational system.</strong> Looking into the next decade, the education industry will continue to face involving challenges in shaping its relationships with the communities they serve. Changes in technologies, commerce, politics and demographics will require educational systems to adapt to serve their citizenry and to contribute to the future economic success of nations, states and regions.</p>
+
+                                <ul>
+                                    <li><a href="#">Career Counselling</a></li>
+                                    <li><a href="#">Cloud Computing</a></li>
+                                    <li><a href="#">Infrastructure Services</a></li>
+                                    <li><a href="#">Research & Development</a></li>
+                                </ul>
+                            </div>
+                            <div class="cl">&nbsp;</div>
+                        </section>
+
+                        <section class="testimonial">
+                            <h2>This is all about Technoflairlab</h2>
+
+                            <p><strong class="quote">“</strong>A chance to blot our mistakes and change failure into winning. It does not take a special time to make a brand new start. It only takes the deep desire to try with all our heart.<br> To live a little better, to always be forgiving. To add a little sunshine in a world for which were living. Never give up in despair Nor think you are through. For theres always a tomorrow, a chance to start new.</p>
+
+                            <p class="author">Ashish Sharma, <strong>TechnoFlairLab</strong></p>
+                        </section>
+                    </div>
+                    <!-- end of main -->
+
+                    <!-- footer-->
+                    <?php include './comp/footer.php' ?>;
+                    <!--footer ends-->
+                </div>
+                <!-- end of container -->	
+            </div>
+            <!-- end of shell -->	
+        </div>
+        <!-- end of wrapper -->
+    </body>
+</html>
