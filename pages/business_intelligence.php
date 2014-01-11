b04188e9450adc3d69f20698f575472e55440213
neo <neo@neo-Aspire-V3-551G>
2014-01-11 18:09:23 +0530

    -- New Pages for content as per the document.

---
+++ pages/business_intelligence.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+     * Business Intelligence doesn’t have any sub menu in it. It has a separate page for itself, and this page has four sections IBM Cognos, ROAMBI, Qlik View and TM1 and their content.
+i) IBM Cognos
+IBM Cognos has two section in the same page Dashboarding and Mobility and their content.
+ii) ROAMBI
+ROAMBI has its content in the page.
+iii) Qlik View
+Qlik View has its content in the page.
+iv) TM1
+TM1 has its content in the page.
+     */
+?>
---
+++ pages/research.php
@@ -0,0 +1,8 @@
+<?php
+    //Research and Development.
+    //Innovation Section
+        //Team Passionate about ideas
+        //e-Recovery
+    //Solutions for you Section
+?>
+
