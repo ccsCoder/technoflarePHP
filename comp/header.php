db1d84c167ae4e9fe5b2138c256f6f431455b015
neo <neo@neo-Aspire-V3-551G>
2014-01-20 22:22:21 +0530

    -- removed search ( non functional right now )

--- comp/header.php
+++ comp/header.php
@@ -14,12 +14,12 @@
 <header id="header">
     <h1 id="logo"><a href=<?php echo $urlPrefix."index" ?>>Techno Flair Lab</a></h1>
     <!-- search -->
-    <div class="search">
+<!--    <div class="search">
         <form method="post">
             <span class="field"><input type="text" class="field" value="keywords here ..." title="keywords here ..." /></span>
             <input type="submit" class="search-btn" value="" />
         </form>
-    </div>
+    </div>-->
     <!-- end of search -->
 </header>
 <!-- end of header -->
